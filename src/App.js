e45ce02d250e48eff76086964758df19527f8f2f
Hunter <no@no.com>
2021-09-07 15:44:26 -0400

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
 class App extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
       title: 'Anh Cu',
       headerLinks: [
-        {title: 'Home', path: '/'},
-        {title: 'About', path: '/about'},
-        {title: 'Contact', path: '/contact'}
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact', path: '/contact' }
       ],
       home: {
         title: 'Hello',
@@ -27,14 +30,23 @@ class App extends React.Component {
     }
   }
 
-
-
-
   render() {
   return (
     <Router>
-      <Container fluid={false}>
-        <p>hello world</p>
+      <Container className="p-0" fluid={true}>
+
+        <Navbar className="border-bottom" bg="transparent" expand="lg">
+          <Navbar.Brand>Anh Cu</Navbar.Brand>
+          
+          <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+           <Navbar.Collapse id="navbar-toggle">
+              <Nav>
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+        </Navbar>
       </Container>
     </Router>
   );
