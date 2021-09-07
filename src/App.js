25b635566fdcd83d3f63bc7c1901a80f570f285e
Hunter <no@no.com>
2021-09-07 11:56:48 -0400

    add path and install react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,32 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Anh Cu',
+      headerLinks: [
+        {title: 'Home', path: '/'},
+        {title: 'About', path: '/about'},
+        {title: 'Contact', path: '/contact'}
+      ],
+      home: {
+        title: 'Hello',
+        subTitle: "I'm a Web Developer",
+        text: 'Check out my recent project'
+      },
+      about: {
+        title: 'Get to know me'
+      },
+      contact: {
+        title: 'Let\'s talk'
+      }
+    }
+  }
+
+
+
+
   render() {
   return (
     <div className="nav">Hello world....</div>
