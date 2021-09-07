b820870edbbd1ec75a899bada82b93d02bece5e3
Hunter <no@no.com>
2021-09-07 13:24:17 -0400

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 import './App.css';
 
 class App extends React.Component {
@@ -30,7 +32,11 @@ class App extends React.Component {
 
   render() {
   return (
-    <div className="nav">Hello world....</div>
+    <Router>
+      <Container fluid={false}>
+        <p>hello world</p>
+      </Container>
+    </Router>
   );
   }
 }
