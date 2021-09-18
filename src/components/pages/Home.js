511a95a1c23a8982f779665942d9c2b0c3931c9e
Hunter <no@no.com>
2021-09-17 23:48:50 -0400

    finish

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -13,11 +13,12 @@ import '../styles/Home.css';
 
 function Home() {
   return (
-    <div className='bg'>
-      <div className='header-title'>
-        <h1 className='name'>Anh Cu</h1>
-        <p className='web'> Web Developer</p>
-        {/* <div className='link'>
+    <div className='titleName'>
+      <div className='bg'>
+        <div className='header-title'>
+          <h1 className='name'>Anh Cu</h1>
+          <p className='web'> Web Developer</p>
+          {/* <div className='link'>
           <ul>
             <a href="https://github.com/anhcu"><i className="fa fa-github"></i></a>
             <a href="https://www.linkedin.com/in/anh-cu/"><i className="fa fa-linkedin"></i></a>
@@ -25,6 +26,7 @@ function Home() {
             <a href="https://docs.google.com/document/d/12aLqDhG_LwmI-iwMubeUAxy4WYtasoBdI4JusfkhF8M/edit?usp=sharing"><i className="fa fa-address-card"></i></a>
           </ul>
         </div> */}
+        </div>
       </div>
     </div>
   );
